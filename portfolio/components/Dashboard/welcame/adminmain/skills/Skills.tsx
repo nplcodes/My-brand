eb380512313ca810f98e121cc72db3b60040bff1
NPL Codes <145145606+nplcodes@users.noreply.github.com>
2023-12-18 15:55:57 +0200

    Skills rendering

--- portfolio/components/Dashboard/welcame/adminmain/skills/Skills.tsx
+++ portfolio/components/Dashboard/welcame/adminmain/skills/Skills.tsx
@@ -1,151 +1,51 @@
 "use client"
 
-import Image from "next/image"
-import { motion } from 'framer-motion';
-import Topbar from "../../admintopbar/Topbar";
+
+import { useState } from "react";
+import SkillsList from "./subcomponents/SkillsList";
+import NewSkill from "../Forms/NewSkill";
+import { MdOutlineFilterList } from "react-icons/md";
+import { IoMdAdd } from "react-icons/io";
 
 
 const Skills = () => {
+  const [renderDiv, setenderDiv] = useState<String>('works')
+  const handleOnclick = (selected: string) =>{
+    setenderDiv(selected)
+  }
+  const RenderSelectedComponent = () =>{
+    switch (renderDiv) {
+      case 'skills':
+        return <SkillsList />
+        break;
+      case 'new':
+        return <NewSkill />
+        break;
+
+      default:
+        return "No selected div"
+        break;
+    }
+  }
   return (
     <div>
         <div className="flex justify-center items-center py-5 gap-10">
           <div className="header-personal">SKILLS + KNOWLDGE</div>
           <div className="main-content px-1">
-            <Topbar />
+          <div>
+            <main className="flex">
+                <div onClick={()=> handleOnclick('skills')} className="menu flex flex-row items-center gap-3 rounded-md cursor-pointer hover:bg-blue-500  hover:text-white hover:font-bold">
+                    <span className="text-xl"><MdOutlineFilterList /></span>
+                </div>
+                <div onClick={()=> handleOnclick('new')} className="menu flex flex-row items-center gap-3 rounded-md cursor-pointer hover:bg-blue-500 hover:font-bold hover:text-white">
+                    <span className="text-xl font-bold"><IoMdAdd /></span>
+                </div>
+            </main>
+          </div>
           </div>
        </div>
        <div className="wrapper">
-        {/* grid grid-cols-3 px-32 py-10 */}
-        <div className="all-skillls-container lg:grid lg:grid-cols-2 lg:px-32 lg:gap-2 lg:py-10 xl:grid-cols-3">
-          <motion.div
-            whileHover={{scale:1.1}}
-            transition={{duration:0.5}}
-           className="one-skill grid grid-cols-4 gap-3  py-10 px-5 rounded-tl-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <Image alt=""  src='/images/react.png' width={70} height={70} className="rounded-md"/>
-            <div className="title-percent-bar col-span-3">
-              <div className="title-percent flex justify-between pb-2">
-                <div>React</div>
-                <div>95%</div>
-              </div>
-              <div className="bar bg-gray-500 h-2 relative ">
-                <div className="bar-color bg-blue-500 w-[80%] h-2 absolute top-0 left-0"></div>
-              </div>
-            </div>
-          </motion.div>
-
-          <motion.div
-           whileHover={{scale:1.1}}
-           transition={{duration:0.5}}
-           className="one-skill grid grid-cols-4 gap-3  py-10 px-5 rounded-tl-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <Image alt=""  src='/images/next.png' width={70} height={70} className="rounded-md"/>
-            <div className="title-percent-bar col-span-3">
-              <div className="title-percent flex justify-between pb-2">
-                <div>Next</div>
-                <div>95%</div>
-              </div>
-              <div className="bar bg-gray-500 h-2 relative ">
-                <div className="bar-color bg-blue-500 w-[80%] h-2 absolute top-0 left-0"></div>
-              </div>
-            </div>
-          </motion.div>
-
-          <div className="one-skill grid grid-cols-4 gap-3  py-10 px-5 rounded-tl-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <Image alt=""  src='/images/postgres.png' width={70} height={70} className="rounded-md"/>
-            <div className="title-percent-bar col-span-3">
-              <div className="title-percent flex justify-between pb-2">
-                <div>Postgres</div>
-                <div>95%</div>
-              </div>
-              <div className="bar bg-gray-500 h-2 relative ">
-                <div className="bar-color bg-blue-500 w-[80%] h-2 absolute top-0 left-0"></div>
-              </div>
-            </div>
-          </div>
-
-          <motion.div 
-             whileHover={{scale:1.1}}
-             transition={{duration:0.5}}
-          className="one-skill grid grid-cols-4 gap-3  py-10 px-5 rounded-tl-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <Image alt=""  src='/images/python.png' width={70} height={70} className="rounded-md"/>
-            <div className="title-percent-bar col-span-3">
-              <div className="title-percent flex justify-between pb-2">
-                <div>Node</div>
-                <div>95%</div>
-              </div>
-              <div className="bar bg-gray-500 h-2 relative ">
-                <div className="bar-color bg-blue-500 w-[80%] h-2 absolute top-0 left-0"></div>
-              </div>
-            </div>
-          </motion.div>
-
-          <div className="one-skill grid grid-cols-4 gap-3  py-10 px-5 rounded-tl-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <Image alt=""  src='/images/react.png' width={70} height={70} className="rounded-md"/>
-            <div className="title-percent-bar col-span-3">
-              <div className="title-percent flex justify-between pb-2">
-                <div>React</div>
-                <div>95%</div>
-              </div>
-              <div className="bar bg-gray-500 h-2 relative ">
-                <div className="bar-color bg-blue-500 w-[80%] h-2 absolute top-0 left-0"></div>
-              </div>
-            </div>
-          </div>
-
-          <div className="one-skill grid grid-cols-4 gap-3  py-10 px-5 rounded-tl-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <Image alt=""  src='/images/docker.png' width={70} height={70} className="rounded-md"/>
-            <div className="title-percent-bar col-span-3">
-              <div className="title-percent flex justify-between pb-2">
-                <div>Docker</div>
-                <div>95%</div>
-              </div>
-              <div className="bar bg-gray-500 h-2 relative ">
-                <div className="bar-color bg-blue-500 w-[80%] h-2 absolute top-0 left-0"></div>
-              </div>
-            </div>
-          </div>
-
-          <motion.div
-            whileHover={{scale:1.1}}
-            transition={{duration:0.5}}
-           className="one-skill grid grid-cols-4 gap-3  py-10 px-5 rounded-tl-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <Image alt=""  src='/images/ts.png' width={70} height={70} className="rounded-md"/>
-            <div className="title-percent-bar col-span-3">
-              <div className="title-percent flex justify-between pb-2">
-                <div>TypeScript</div>
-                <div>95%</div>
-              </div>
-              <div className="bar bg-gray-500 h-2 relative ">
-                <div className="bar-color bg-blue-500 w-[80%] h-2 absolute top-0 left-0"></div>
-              </div>
-            </div>
-          </motion.div>
-
-          <div className="one-skill grid grid-cols-4 gap-3  py-10 px-5 rounded-tl-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <Image alt=""  src='/images/django.png' width={70} height={70} className="rounded-md"/>
-            <div className="title-percent-bar col-span-3">
-              <div className="title-percent flex justify-between pb-2">
-                <div>Django</div>
-                <div>95%</div>
-              </div>
-              <div className="bar bg-gray-500 h-2 relative ">
-                <div className="bar-color bg-blue-500 w-[80%] h-2 absolute top-0 left-0"></div>
-              </div>
-            </div>
-          </div>
-
-          <div className="one-skill grid grid-cols-4 gap-3  py-10 px-5 rounded-tl-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-            <Image alt=""  src='/images/figma.png' width={70} height={70} className="rounded-md"/>
-            <div className="title-percent-bar col-span-3">
-              <div className="title-percent flex justify-between pb-2">
-              <div>Figma</div>
-                <div>95%</div>
-              </div>
-              <div className="bar bg-gray-500 h-2 relative ">
-                <div className="bar-color bg-blue-500 w-[80%] h-2 absolute top-0 left-0"></div>
-              </div>
-            </div>
-          </div>
-        </div>
+        {RenderSelectedComponent()}
        </div>
     </div>
   )

b2dbc542d2701831e5c020d473b0caa0866533ef
NPL Codes <145145606+nplcodes@users.noreply.github.com>
2023-12-29 16:46:54 +0200

    backend

--- portfolio/components/works/subcomponents/Web.tsx
+++ portfolio/components/works/subcomponents/Web.tsx
@@ -1,4 +1,8 @@
-import { useEffect, useState } from "react";
+"use client"
+
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+
 
 interface Work {
   title: string;
@@ -7,7 +11,6 @@ interface Work {
   colaborated: string;
   image: File | string | null;
 }
-
 
 const Web = () => {
   const [data, setdata] = useState<Work[]>([]);
@@ -31,19 +34,34 @@ const Web = () => {
   }, []);
   return (
     // grid grid-cols-4 gap-2 min-h-[500px] p-5 px-10
-    <div className="works grid grid-cols-1 gap-4 min-h-[1000px] p-5 px-10">
+    <div className="works grid grid-cols-1 gap-4 min-h-[1000px] p-5 px-10 lg:grid-cols-2 lg:gap-2 lg:px-16 xl:grid-cols-4 xl:min-h-[500px]">
       {/* grid grid-rows-2 gap-1 */}
-        {/* <div className="right-small grid grid-cols-1 gap-1"> */}
-            <div className="bg-cover bg-no-repeat bg-center rounded-md" style={{backgroundImage: `url(${data[0]?.image})`,}}></div>
-            <div className="bg-cover bg-no-repeat bg-center rounded-md" style={{backgroundImage: 'url(/images/w3.jpg)',}}></div>
-        {/* </div> */}
+        <div className="right-small xl:grid xl:grid-cols-1 xl:gap-1">
+            <motion.div
+              whileHover={{scale:1.05}}
+              transition={{duration:0.5}} 
+              className="bg-cover bg-no-repeat bg-center rounded-md cursor-pointer" style={{backgroundImage: `url(${data[0]?.image})`,}}>
+            </motion.div>
+            <motion.div
+              whileHover={{scale:1.05}}
+              transition={{duration:0.5}} 
+              className="bg-cover bg-no-repeat bg-center rounded-md cursor-pointer" style={{backgroundImage: `url(${data[1]?.image})`,}}>
+            </motion.div>
+        </div>
         {/* col-span-2 */}
-        <div className="middle-large bg-cover bg-no-repeat bg-center rounded-md" style={{backgroundImage: 'url(/images/w1.webp)',}}></div>
-        {/* <div className="left-small grid grid-rows-3 gap-1"> */}
-        <div className="bg-cover bg-no-repeat bg-center rounded-md" style={{backgroundImage: 'url(/images/w1.jpg)',}}></div>
-        {/* <div className="bg-cover bg-no-repeat bg-center" style={{backgroundImage: 'url(/images/w1.jpg)',}}></div>
-        <div className="bg-cover bg-no-repeat bg-center" style={{backgroundImage: 'url(/images/w1.jpg)',}}></div> */}
-        {/* </div> */}
+        <motion.div whileHover={{scale:1.2}} transition={{duration:0.6}}  className="middle-large bg-cover bg-no-repeat bg-center rounded-md xl:col-span-2 cursor-pointer" style={{backgroundImage: 'url(/images/w1.jpg)',}}></motion.div>
+          <div className="left-small xl:grid xl:grid-rows-3 xl:gap-1">
+          <motion.div 
+            whileHover={{scale:1.05}}
+            transition={{duration:0.5}} 
+          className="bg-cover bg-no-repeat bg-center cursor-pointer rounded-md" style={{backgroundImage: `url(${data[2]?.image})`,}}>
+          </motion.div>
+          <motion.div
+            whileHover={{scale:1.05}}
+            transition={{duration:0.5}} 
+          className="bg-cover bg-no-repeat bg-center cursor-pointer" style={{backgroundImage: `url(${data[3]?.image})`,}}>
+          </motion.div>
+        </div>
   </div>
   )
 }
